cb74c2584c22606ee0862a1d02674c4155541046
xiaojie <1624565996@qq.com>
2026-01-15 19:17:52 +0800

    2026-1-15-2

---
+++ market-product/src/main/java/com/xiaojie/NFTMarketProductApplication.java
@@ -0,0 +1,12 @@
+package com.xiaojie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NFTMarketProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NFTMarketProductApplication.class, args);
+    }
+}
